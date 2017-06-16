8ce3b8b1f528f2d262f949fbb3e6fc976fe650fb
Rahul Krishna <rkbalu524@gmail.com>
2017-06-16 14:47:14 +0530

    Test run with real iimages

--- script.js
+++ script.js
@@ -3,31 +3,26 @@ $('#girl').animatedModal({
   modalTarget:'girlModal',
   animatedIn:'bounceInUp',
   animatedOut:'bounceOutDown',
-  // Callbacks
-  beforeOpen: function() {
-      console.log("The animation was called");
-  },
-  afterOpen: function() {
-      console.log("The animation is completed");
-  },
-  beforeClose: function() {
-      console.log("The animation was called");
-  },
-  afterClose: function() {
-      console.log("The animation is completed");
-  }
+  color:'#000'
 })
 
-$('#legend').animatedModal({
-  modalTarget:'legendModal',
+$('#woman').animatedModal({
+  modalTarget:'womanModal',
+  animatedIn:'bounceInDown',
+  animatedOut:'bounceOutUp',
+  color:'#000'
+})
+
+$('#lover').animatedModal({
+  modalTarget:'loverModal',
   animatedIn:'rubberBand',
   animatedOut:'flipOutX',
-  color:'#3498db'
+  color:'#000'
 })
 
 $('#professional').animatedModal({
   modalTarget:'professionalModal',
   animatedIn:'lightSpeedIn',
   animatedOut:'lightSpeedOut',
-  color:'#3498db'
+  color:'#000'
 })
