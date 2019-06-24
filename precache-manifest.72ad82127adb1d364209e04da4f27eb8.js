ff18f9982ad1b15e09b7dbef047ac774db1df2ad
Rahul Krishna <rkbalu524@gmail.com>
2019-06-24 22:59:47 +0530

    Somewhat final build

---
+++ precache-manifest.72ad82127adb1d364209e04da4f27eb8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0a5ac11a38ceab67c3289883443c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9812bc3d047c60d28de",
+    "url": "/static/css/2.38695677.chunk.css"
+  },
+  {
+    "revision": "b138ca622ef3e4d9e1df",
+    "url": "/static/css/main.7eade0e2.chunk.css"
+  },
+  {
+    "revision": "c9812bc3d047c60d28de",
+    "url": "/static/js/2.2503653b.chunk.js"
+  },
+  {
+    "revision": "b138ca622ef3e4d9e1df",
+    "url": "/static/js/main.bfcc0b81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03be35eff8e2ecebf0fb34219fe8a068",
+    "url": "/static/media/1.03be35ef.jpg"
+  },
+  {
+    "revision": "a6326113a618a4721c6562bee9683177",
+    "url": "/static/media/2.a6326113.jpg"
+  },
+  {
+    "revision": "28b3de98a7d0166c516397ee2d0cfbf2",
+    "url": "/static/media/3.28b3de98.jpg"
+  },
+  {
+    "revision": "1eb780689a5c5c4111cfdb3297083dd1",
+    "url": "/static/media/4.1eb78068.jpg"
+  },
+  {
+    "revision": "3fce12d065a103d8f7a1936189c14f7f",
+    "url": "/static/media/5.3fce12d0.jpg"
+  },
+  {
+    "revision": "8e0637b5b9f5e4d20ee17abc2ca87dd8",
+    "url": "/static/media/6.8e0637b5.jpg"
+  },
+  {
+    "revision": "d514360bdb8363bf0066919b7eab4fe5",
+    "url": "/static/media/7.d514360b.jpg"
+  },
+  {
+    "revision": "9a6c8fa4569bf48f26fdef073ef00615",
+    "url": "/static/media/8.9a6c8fa4.jpg"
+  }
+]);
